02d22c65d8466fba157f9bc92ac60f91385a408d
Mowalky <mowalky@outlook.com>
2022-05-27 10:57:43 -0500

    load data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import path from "path";
+import fs from "fs/promises";
+
 function HomePage(props) {
   const { products } = props;
   return (
     <div>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
+          <li key={product.id}>{product.title}</li>
         ))}
       </ul>
     </div>
@@ -12,9 +15,12 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
   return {
     props: {
-      products: [{ id: 1, name: "name1" }],
+      ...data,
     },
   };
 }
